50b2dcd64f80c2a6e8ff4114b0ba14bf67636158
Søren Magnusson <smag@tec.dk>
2025-11-07 11:20:04 +0100

    Første (lidt uoverskuelige) wifi eksempel

---
+++ Wifi/README.md
@@ -0,0 +1,41 @@
+Document Language: [English](README.md) | [简体中文](README.zh-cn.md)
+
+# 🌶️ - WiFI samples
+
+Shows how to use various APIs related with Wifi. Scan WiFI Networks and connect to a specified network.
+
+The sample is [located here](./ScanWifi/Program.cs).
+
+## Hardware requirements
+
+An hardware device with Wifi networking capabilities running a nanoFramework image.
+
+## Related topics
+
+### Reference
+
+- [System.Device.Wifi](http://docs.nanoframework.net/api/System.Device.Wifi.html)
+
+## Build the sample
+
+1. Start Microsoft Visual Studio 2022 (VS 2019 should be OK too) and select `File > Open > Project/Solution`.
+1. Starting in the folder where you unzipped the samples/cloned the repository, go to the subfolder for this specific sample. Double-click the Visual Studio Solution (.sln) file.
+1. Press `Ctrl+Shift+B`, or select `Build > Build Solution`.
+
+## Run the sample
+
+The next steps depend on whether you just want to deploy the sample or you want to both deploy and run it.
+
+### Deploying the sample
+
+- Select `Build > Deploy Solution`.
+
+### Deploying and running the sample
+
+- To debug the sample and then run it, press F5 or select `Debug > Start Debugging`.
+
+> [!NOTE]
+>
+> **Important**: Before deploying or running the sample, please make sure your device is visible in the Device Explorer.
+>
+> **Tip**: To display the Device Explorer, go to Visual Studio menus: `View > Other Windows > Device Explorer`.
---
+++ Wifi/ScanWiFi/Program.cs
@@ -0,0 +1,115 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Device.Wifi;
+using nanoFramework.Networking;
+
+namespace ScanWifi
+{
+    public class Program
+    {
+        // Set the SSID & Password to your local Wifi network
+        //const string MYSSID = "MySsdid";
+        //const string MYPASSWORD = "MyPassword";
+        const string MYSSID = "TEC-IOT";
+        const string MYPASSWORD = "42090793";
+
+        public static void Main()
+        {
+            try
+            {
+                // Get the first WiFI Adapter
+                WifiAdapter wifi = WifiAdapter.FindAllAdapters()[0];
+
+                // Set up the AvailableNetworksChanged event to pick up when scan has completed
+                wifi.AvailableNetworksChanged += Wifi_AvailableNetworksChanged;
+
+                // give it some time to perform the initial "connect"
+                // trying to scan while the device is still in the connect procedure will throw an exception
+                Thread.Sleep(10_000);
+
+                // Loop forever scanning every 30 seconds
+                while (true)
+                {
+                    try
+                    {
+                        Debug.WriteLine("starting Wi-Fi scan");
+                        wifi.ScanAsync();
+                        
+                    }
+                    catch(Exception ex)
+                    {
+                        Debug.WriteLine($"Failure starting a scan operation: {ex}");
+                    }
+
+                    Thread.Sleep(30000);
+                    // jeg leger med NTP
+                    // nu flytter jeg nedenstående 4 linier til Wifi_AvailableNetworksChanged (ca. linje 100)
+                    //Sntp.Server1 = "1.europe.pool.ntp.org";
+                    //Sntp.Start();
+
+                    //Debug.WriteLine($"System time: {DateTime.UtcNow}");
+                    //Debug.WriteLine($"Local System time: {DateTime.UtcNow.Kind}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("message:" + ex.Message);
+                Debug.WriteLine("stack:" + ex.StackTrace);
+            }
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        /// <summary>
+        /// Event handler for when Wifi scan completes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void Wifi_AvailableNetworksChanged(WifiAdapter sender, object e)
+        {
+            Debug.WriteLine("Wifi_AvailableNetworksChanged - get report");
+
+            // Get Report of all scanned Wifi networks
+            WifiNetworkReport report = sender.NetworkReport;
+
+            // Enumerate though networks looking for our network
+            foreach (WifiAvailableNetwork net in report.AvailableNetworks)
+            {
+                // Show all networks found
+                Debug.WriteLine( $"Net SSID :{net.Ssid},  BSSID : {net.Bsid},  rssi : {net.NetworkRssiInDecibelMilliwatts.ToString()},  signal : {net.SignalBars.ToString()}");
+
+                // If its our Network then try to connect
+                if (net.Ssid == MYSSID)
+                {
+                    // Disconnect in case we are already connected
+                    sender.Disconnect();
+
+                    // Connect to network
+                    WifiConnectionResult result = sender.Connect(net, WifiReconnectionKind.Automatic, MYPASSWORD);
+
+                    // Display status
+                    if (result.ConnectionStatus == WifiConnectionStatus.Success)
+                    {
+                        Debug.WriteLine("Connected to Wifi network");
+                        Sntp.Server1 = "1.europe.pool.ntp.org";
+                        Sntp.Start();
+
+                        Debug.WriteLine($"System time: {DateTime.UtcNow}");
+                        Debug.WriteLine($"Local System time: {DateTime.UtcNow.Kind}");
+                    }
+                    else
+                    {
+                        Debug.WriteLine($"Error {result.ConnectionStatus.ToString()} connecting o Wifi network");
+                    }
+                }
+            }
+        }
+    }
+}
+
